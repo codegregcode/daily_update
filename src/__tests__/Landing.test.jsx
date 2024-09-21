32b3df62fe5f5619470409a40b35c3b308b15ba7
Greg Cain <codegregcode@gmail.com>
2024-09-21 19:09:20 +0100

    imports Memory Router into tests

--- src/__tests__/Landing.test.jsx
+++ src/__tests__/Landing.test.jsx
@@ -1,31 +1,48 @@
 import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import Landing from '../components/Landing';
 import { expect } from 'vitest';
 
 describe('Landing component', () => {
   it('renders h1', () => {
-    const { getByText } = render(<Landing />);
+    const { getByText } = render(
+      <MemoryRouter>
+        <Landing />
+      </MemoryRouter>
+    );
     const h1Element = getByText('daily_update');
 
     expect(h1Element).toBeInTheDocument();
   });
 
   it('renders login button', () => {
-    const { getByText } = render(<Landing />);
+    const { getByText } = render(
+      <MemoryRouter>
+        <Landing />
+      </MemoryRouter>
+    );
     const loginButton = getByText('Login');
 
     expect(loginButton).toBeInTheDocument();
   });
 
   it('renders register button', () => {
-    const { getByText } = render(<Landing />);
+    const { getByText } = render(
+      <MemoryRouter>
+        <Landing />
+      </MemoryRouter>
+    );
     const registerButton = getByText('Register');
 
     expect(registerButton).toBeInTheDocument();
   });
 
   it('renders forgotten password button', () => {
-    const { getByText } = render(<Landing />);
+    const { getByText } = render(
+      <MemoryRouter>
+        <Landing />
+      </MemoryRouter>
+    );
     const forgottenPasswordButton = getByText('Forgotten Password?');
 
     expect(forgottenPasswordButton).toBeInTheDocument();
