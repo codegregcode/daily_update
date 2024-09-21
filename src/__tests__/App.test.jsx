84aa2c5c53f859428d7899fb52ef12462e8c2500
Greg Cain <codegregcode@gmail.com>
2024-09-21 19:07:41 +0100

    updates App test for react-router-dom and adds to main

--- src/__tests__/App.test.jsx
+++ src/__tests__/App.test.jsx
@@ -1,9 +1,14 @@
 import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import App from '../components/App';
 
 describe('App component', () => {
   it('matches the snapshot', () => {
-    const { asFragment } = render(<App />);
+    const { asFragment } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    );
     expect(asFragment()).toMatchSnapshot();
   });
 });
