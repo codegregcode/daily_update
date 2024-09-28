4c0370bbe91841991d0a7fefd2e27ec1cf7fffa7
Greg Cain <codegregcode@gmail.com>
2024-09-28 18:28:52 +0100

    creates Landing component passing tests

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,18 +1,10 @@
-import { Link } from 'react-router-dom';
+import { signInWithGoogle } from '../firebase';
 
 function Landing() {
   return (
     <>
       <h1>daily_update</h1>
-      <Link key="login" to="/login">
-        <button>Login</button>
-      </Link>
-      <Link key="register" to="/register">
-        <button>Register</button>
-      </Link>
-      <Link key="forgotten password" to="/forgot-password">
-        <button>Forgotten Password?</button>
-      </Link>
+      <button onClick={signInWithGoogle}>Login/SignUp</button>
     </>
   );
 }
