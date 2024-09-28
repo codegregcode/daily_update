9a12339834c70f7934b54271e97ea96497e1a33d
Greg Cain <codegregcode@gmail.com>
2024-09-28 18:29:30 +0100

    imports new component into App and updates snapshot

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
 
 import Landing from './Landing';
-import Login from './Login';
-import Register from './Register';
-import ForgotPW from './ForgotPW';
 
 import '../styles/App.css';
 
@@ -12,9 +9,6 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<Landing />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/forgot-password" element={<ForgotPW />} />
       </Routes>
     </>
   );
