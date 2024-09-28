4ccfe2d98aaf64378480799a6bc6bb0dfb62788a
Greg Cain <codegregcode@gmail.com>
2024-09-28 18:27:37 +0100

    adds signin/register and logout functions

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,21 @@
 import { initializeApp } from 'firebase/app';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import {
+  GoogleAuthProvider,
+  getAuth,
+  signInWithPopup,
+  signOut,
+} from 'firebase/auth';
+
+import {
+  getFirestore,
+  query,
+  getDocs,
+  collection,
+  where,
+  setDoc,
+  doc,
+} from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
@@ -10,5 +27,33 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
 
-export default app;
+const googleProvider = new GoogleAuthProvider();
+
+const signInWithGoogle = async () => {
+  try {
+    const res = await signInWithPopup(auth, googleProvider);
+    const { user } = res;
+    const q = query(collection(db, 'users'), where('uid', '==', user.uid));
+    const docs = await getDocs(q);
+    if (docs.docs.length === 0) {
+      await setDoc(doc(db, 'users', user.uid), {
+        uid: user.uid,
+        name: user.displayName,
+        authProvider: 'google',
+        email: user.email,
+        role: 'agent',
+      });
+    }
+  } catch (err) {
+    console.error(err.message);
+  }
+};
+
+const logout = () => {
+  signOut(auth);
+};
+
+export { auth, useAuthState, signInWithGoogle, logout };
