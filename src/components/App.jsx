9953c8950cdf065c82150f3d1ce39c33d1b1720d
Greg Cain <codegregcode@gmail.com>
2024-10-03 19:17:45 +0100

    imports DashboardContainer into App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 
 import Landing from './Landing';
+import DashboardContainer from './DashboardContainer';
 
 import '../styles/App.css';
 
@@ -9,6 +10,7 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<Landing />} />
+        <Route path="/dashboard" element={<DashboardContainer />} />
       </Routes>
     </>
   );
