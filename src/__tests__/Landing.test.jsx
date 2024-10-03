8c8fb4e63071e07dfda6b986e9053a3738b0bd54
Greg Cain <codegregcode@gmail.com>
2024-10-03 19:09:23 +0100

    updates Landing test with mocks and test for navigation

--- src/__tests__/Landing.test.jsx
+++ src/__tests__/Landing.test.jsx
@@ -1,9 +1,32 @@
 import { render } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import Landing from '../components/Landing';
-import { expect } from 'vitest';
+import { expect, vi, beforeEach } from 'vitest';
+import { useAuthState } from '../firebase';
+import { useNavigate } from 'react-router-dom';
+
+vi.mock('../firebase', () => ({
+  signInWithGoogle: vi.fn(),
+  auth: {},
+  useAuthState: vi.fn(),
+}));
+
+vi.mock('react-router-dom', async (importOriginal) => {
+  const actual = await importOriginal();
+  return {
+    ...actual,
+    useNavigate: vi.fn(),
+  };
+});
 
 describe('Landing component', () => {
+  const navigate = vi.fn();
+
+  beforeEach(() => {
+    useAuthState.mockReturnValue([null, false]);
+    useNavigate.mockReturnValue(navigate);
+  });
+
   it('renders h1', () => {
     const { getByText } = render(
       <MemoryRouter>
@@ -24,5 +47,17 @@ describe('Landing component', () => {
 
     const loginButton = getByText('Login/SignUp');
     expect(loginButton).toBeInTheDocument();
+  });
+
+  it('navigates to /dashboard if user is logged in', async () => {
+    useAuthState.mockReturnValue([{ uid: '123' }, false]); // Simulate user logged in
+
+    render(
+      <MemoryRouter>
+        <Landing />
+      </MemoryRouter>
+    );
+
+    expect(navigate).toHaveBeenCalledWith('/dashboard');
   });
 });
